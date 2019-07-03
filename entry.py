a26fc710d185a7b2b206ceb0c34ebbc307c4c7d3
Anton Ustyuzhanin <au@hellofresh.com>
2019-07-03 10:02:44 +0200

    Search for environment in rds tags too

--- entry.py
+++ entry.py
@@ -7,12 +7,12 @@ import yaml
 import subprocess
 
 
-REGION = 'eu-west-1'
+AWS_REGION = os.getenv('AWS_REGION', 'eu-west-1')
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'staging')
 
+client = boto3.client('rds', region_name=AWS_REGION)
 
-def get_db_instances(region, page_size=20):
-    client = boto3.client('rds', region_name=region)
+def get_db_instances(page_size=20):
     marker = ""
     pool = []
     while True:
@@ -27,14 +27,29 @@ def get_db_instances(region, page_size=20):
         pool = result.get("DBInstances")
 
 
+def get_rds_tags(instance_arn):
+    response = client.list_tags_for_resource(
+        ResourceName=instance_arn
+    )
+    return { tag['Key']: tag['Value'] for tag in response['TagList'] }
+
+
+def is_applicable(instance):
+    instance_name = instance['DBInstanceIdentifier']
+    instance_arn = instance['DBInstanceArn']
+    instance_tags = get_rds_tags(instance_arn)
+    instance_enhanced_monitoring_arn = instance.get('EnhancedMonitoringResourceArn', None)
+    instance_environment_tag = instance_tags.get('Environment', '')
+    return instance_enhanced_monitoring_arn and (instance_name.lower().endswith(ENVIRONMENT.lower()) or instance_environment_tag.lower() == ENVIRONMENT.lower())
+
+
 if __name__ == "__main__":
     em_enabled_instances = {'instances': []}
 
-    for instance in get_db_instances(REGION):
-        instance_name = instance['DBInstanceIdentifier']
-        if (instance.get('EnhancedMonitoringResourceArn', None) and
-                instance_name.endswith(ENVIRONMENT)):
-            instance = {'instance': instance_name, 'region': REGION}
+    for instance in get_db_instances():
+        if is_applicable(instance):
+            instance_name = instance['DBInstanceIdentifier']
+            instance = {'instance': instance_name, 'region': AWS_REGION}
             em_enabled_instances['instances'].append(instance)
 
     with open('/rds_exporter/config.yml', 'w') as yaml_file:
